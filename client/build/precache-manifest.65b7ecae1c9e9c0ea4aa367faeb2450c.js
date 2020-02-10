05a881ad0e2d7ec33beb1cb002e147b86b3fc4a2
Shan Wong <shan.wong29@gmail.com>
2020-02-10 20:24:31 +0100

    deploy to herouku

---
+++ client/build/precache-manifest.65b7ecae1c9e9c0ea4aa367faeb2450c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ff891d6d5158cd0ecd7cd490d5fe35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "200618859a22eafb3421",
+    "url": "/static/css/main.5c731fee.chunk.css"
+  },
+  {
+    "revision": "83c89f4badd24e4c850f",
+    "url": "/static/js/2.fbaec6cd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.fbaec6cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "200618859a22eafb3421",
+    "url": "/static/js/main.25b9697a.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
