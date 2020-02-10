4e81ab1e87bc37d8f0e07775bf37dda330ca7a62
Shan Wong <shan.wong29@gmail.com>
2020-02-10 13:31:02 +0100

    add navbar and include Mosaic component in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { Route } from "react-router-dom";
 import "./App.css";
 import Carousel from "./components/carousel/Carousel";
+import Mosaic from "./components/mosaic/Mosaic";
 import getPhotoData from "./service/getPhotoData";
 
 class App extends React.Component {
   state = {
     photoData: "",
-    activeImgIndex: 0
+    activeImgIndex: 0,
+    mode: "mosaic",
+    isPopUp: false
   };
 
   componentDidMount() {
@@ -34,23 +36,68 @@ class App extends React.Component {
     this.setState({
       activeImgIndex: imgIndex
     });
+    if (this.state.mode === "mosaic") {
+      this.setState({
+        isPopUp: true
+      });
+    }
+  };
+
+  handleModeChange = mode => {
+    this.setState({ mode });
+
+    // close popUp when unmount mosaic component
+    if (this.state.mode === "mosaic") {
+      this.setState({
+        isPopUp: false
+      });
+    }
+  };
+
+  closePopUp = () => {
+    this.setState({ isPopUp: false, activeImgIndex: 0 });
   };
 
   render() {
     return (
       <div className="App">
-        <Route
-          exact
-          path="/"
-          render={props => (
-            <Carousel
-              photoData={this.state.photoData}
-              activeImgIndex={this.state.activeImgIndex}
-              handlePhotoChange={this.handlePhotoChange}
-              err={this.state.err}
-            />
-          )}
-        />
+        <div className="nav">
+          <button
+            className={`nav__modeBtn ${
+              this.state.mode === "mosaic" ? "active" : ""
+            }`}
+            onClick={() => this.handleModeChange("mosaic")}
+          >
+            mosaic
+          </button>
+          <button
+            className={`nav__modeBtn ${
+              this.state.mode === "carousel" ? "active" : ""
+            }`}
+            onClick={() => this.handleModeChange("carousel")}
+          >
+            carousel
+          </button>
+        </div>
+
+        {this.state.mode === "mosaic" && (
+          <Mosaic
+            photoData={this.state.photoData}
+            activeImgIndex={this.state.activeImgIndex}
+            handlePhotoChange={this.handlePhotoChange}
+            err={this.state.err}
+            isPopUp={this.state.isPopUp}
+            closePopUp={this.closePopUp}
+          />
+        )}
+        {this.state.mode === "carousel" && (
+          <Carousel
+            photoData={this.state.photoData}
+            activeImgIndex={this.state.activeImgIndex}
+            handlePhotoChange={this.handlePhotoChange}
+            err={this.state.err}
+          />
+        )}
       </div>
     );
   }
