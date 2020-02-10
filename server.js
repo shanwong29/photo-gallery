8d5cba51a751da590791be544f778671dc999891
Shan Wong <shan.wong29@gmail.com>
2020-02-10 21:10:59 +0100

    add config for deployment

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const axios = require("axios");
-require("dotenv").config();
-const port = 5005;
+const path = require("path");
 
+const port = process.env.PORT || 5005;
 const ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY;
 
 const query = "girl";
@@ -20,6 +21,14 @@ app.get("/api/photos", async (req, res) => {
     res.json({ err: err.message });
   }
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`server is listening on ${port}`);
