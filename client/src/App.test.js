7454112dd13baebb8aaebf6510d0a8022dc0d459
Shan Wong <shan.wong29@gmail.com>
2020-03-30 21:30:59 +0200

    add test for  function and clean up App test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -21,12 +21,12 @@ describe("App test", () => {
     });
 
     //Check initial state
-    it("activeImg in initial State should be the first image", () => {
+    it("`activeImgIndex` in initial State should be 0 (show first image)", () => {
       const state = component.instance().state;
       expect(state.activeImgIndex).toEqual(0);
     });
 
-    it("popUp should be closed", () => {
+    it("popUp initial state should be false", () => {
       const state = component.instance().state;
       expect(state.isPopUp).toEqual(false);
     });
@@ -43,8 +43,8 @@ describe("App test", () => {
 
   // you may check the mock axios function in `./service/__mocks__/axios.js` for reference
   describe("callback functions", () => {
-    describe("when getPhotoData function is called", () => {
-      it("should fetch Photo data from api and update the state correspondingly", () => {
+    describe("when App component is mounted", () => {
+      it("should fetch photo data from api and update the state correspondingly", () => {
         component.unmount();
         component = shallow(<App />);
         setTimeout(() => {
@@ -59,47 +59,39 @@ describe("App test", () => {
       });
     });
 
-    describe("when handlePhotoChange function is called", () => {
-      let instance;
-      const mockActiveImgIndex = 4;
-
-      beforeEach(() => {
-        instance = component.instance();
+    describe("when `handlePhotoChange` function is called", () => {
+      it("should update the `activeImgIndex` property in state", () => {
+        const mockActiveImgIndex = Math.floor(Math.random() * 10);
+        const instance = component.instance();
         instance.handlePhotoChange(mockActiveImgIndex);
-      });
-
-      it("should update the activeImgIndex property in state", () => {
         expect(instance.state.activeImgIndex).toEqual(mockActiveImgIndex);
       });
     });
 
-    describe("when mode = carousel & handlePhotoChange function is called", () => {
+    describe("when mode = `carousel` & `handlePhotoChange` function is called", () => {
       it("popUp should remain close", () => {
         const instance = component.instance();
-        const mockMode = "carousel";
-        const mockActiveImgIndex = 2;
-
-        instance.handleModeChange(mockMode);
+        instance.state.mode = "carousel";
+        const mockActiveImgIndex = Math.floor(Math.random() * 10);
         instance.handlePhotoChange(mockActiveImgIndex);
 
         expect(instance.state.isPopUp).toEqual(false);
       });
     });
 
-    describe("when mode = Mosaic & handlePhotoChange function is called", () => {
+    describe("when mode = `mosaic` & `handlePhotoChange` function is called", () => {
       it("popUp should open", () => {
         const instance = component.instance();
-        const mockMode = "mosaic";
-        const mockActiveImgIndex = 2;
+        instance.state.mode = "mosaic";
+        const mockActiveImgIndex = Math.floor(Math.random() * 10);
 
-        instance.handleModeChange(mockMode);
         instance.handlePhotoChange(mockActiveImgIndex);
 
         expect(instance.state.isPopUp).toEqual(true);
       });
     });
 
-    describe("when handleModeChange function is called to change mode to carousel", () => {
+    describe("when `handleModeChange` function is called to change mode to `carousel`", () => {
       let instance;
       const mockMode = "carousel";
 
@@ -108,7 +100,7 @@ describe("App test", () => {
         instance.handleModeChange(mockMode);
       });
 
-      it("should update the mode property in state", () => {
+      it("should update the `mode` property in state", () => {
         expect(instance.state.mode).toEqual(mockMode);
       });
 
@@ -118,18 +110,41 @@ describe("App test", () => {
       });
     });
 
-    describe("when handleModeChange function is called to change mode to mosaic", () => {
+    describe("when `handleModeChange` function is called to change mode to `mosaic`", () => {
       let instance;
       const mockMode = "mosaic";
 
       beforeEach(() => {
         instance = component.instance();
+        instance.state.isPopUp = true;
         instance.handleModeChange(mockMode);
       });
 
       it("should render Mosaic component", () => {
         expect(component.find("Carousel").length).toEqual(0);
         expect(component.find("Mosaic").length).toEqual(1);
+      });
+
+      it("should close popUp", () => {
+        expect(instance.state.isPopUp).toEqual(false);
+      });
+    });
+
+    describe("when `closePopUp` function is called", () => {
+      let instance;
+      beforeEach(() => {
+        instance = component.instance();
+        instance.state.isPopUp = true;
+        instance.state.activeImgIndex = 2;
+        instance.closePopUp();
+      });
+
+      it("should set the `activeImgIndex` to 0 (first img)", () => {
+        expect(instance.state.activeImgIndex).toEqual(0);
+      });
+
+      it("should set the `isPopUp` to false", () => {
+        expect(instance.state.isPopUp).toEqual(false);
       });
     });
   });
