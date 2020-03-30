d7a177e0d8d82d5a39e16ef6493e6f646dc05a01
Shan Wong <shan.wong29@gmail.com>
2020-03-30 13:42:32 +0200

    change display wordings

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -2,21 +2,20 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = ({ mode, handleModeChange }) => {
-  console.log(mode);
   return (
     <div className="nav">
       <button
         className={`nav__modeBtn ${mode === "mosaic" ? "active" : ""}`}
         onClick={() => handleModeChange("mosaic")}
       >
-        mosaic
+        Mosaic
       </button>
 
       <button
         className={`nav__modeBtn ${mode === "carousel" ? "active" : ""}`}
         onClick={() => handleModeChange("carousel")}
       >
-        carousel
+        Carousel
       </button>
     </div>
   );
