c3374ecfc7f2bdcf1a7810fbf1dc6dc04ccb412a
Shan Wong <shan.wong29@gmail.com>
2020-03-30 21:45:52 +0200

    add more tests for Carousel and clean up existing tests

--- client/src/components/carousel/Carousel.test.js
+++ client/src/components/carousel/Carousel.test.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import Carousel from "./Carousel";
 
 import { render, fireEvent } from "@testing-library/react";
-// import { getByAltText } from "@testing-library/dom";
 
 describe("Carousel Test", () => {
   // *****************************    Below: set up for each test    **********************************
@@ -14,34 +13,36 @@ describe("Carousel Test", () => {
       id: "1",
       alt_description: "aerial photography of city buildings",
       urls: {
-        raw: "https://abc/kjfds",
-        full: "https://fkjs/jsf//dsljf",
-        regular: "https://images.regular-tedt.jpg",
-        small: "https://images.small.jpg",
-        thumb: "https://images=entropy&cWQiOjExNDE3N30"
+        raw: "https://abc.com/kjfds",
+        full: "https://fkjs.com/jsf//dsljf",
+        regular: "https://images.com/regular-tedt.jpg",
+        small: "https://images.small/jd.jpg",
+        thumb: "https://images.com//kjdshf/jks"
       }
     },
     {
       id: "2",
       alt_description: "salt lake",
       urls: {
-        raw: "https://gfsjffds",
-        full: "https://fJUHFd78f",
-        regular: "https://imasfkj87t.jpg",
-        small: "https://imagessfhdsl.jpg",
-        thumb: "https://image43093284ExNDE3N30"
+        raw: "https://gfsjffds.com/kdsafl",
+        full: "https://fJUHFd78f.com/jdsfhs",
+        regular: "https://imasfkj87t.com/jdaslk.jpg",
+        small: "https://imagessfhdsl.com/sjdf",
+        thumb: "https://image43093284ExNDE3N30.com/sjfjs"
       }
     }
   ];
 
-  const mockCallBack = jest.fn();
+  const mockHandlePhotoChange = jest.fn();
+  const randomImgIndex = Math.floor(Math.random() * mockPhotoData.length);
+
   const createTestProps = props => {
     return {
       // common props
       photoData: mockPhotoData,
-      activeImgIndex: 1,
-      handlePhotoChange: mockCallBack,
-      err: null,
+      activeImgIndex: randomImgIndex,
+      handlePhotoChange: mockHandlePhotoChange,
+      err: "",
       ...props
     };
   };
@@ -70,63 +71,114 @@ describe("Carousel Test", () => {
       ReactDOM.unmountComponentAtNode(div);
     });
 
-    it("hide previous-img-button when it is the first img", () => {
-      props = createTestProps({ activeImgIndex: 0 });
-      const { getByTestId } = mockRender(props);
-
-      expect(getByTestId("previous-img-button")).toHaveClass("button__hide");
+    it("should render error msg when there is an error from data fetching", () => {
+      props = createTestProps({ err: "This is a mock error." });
+      const { getAllByText } = mockRender(props);
+      expect(getAllByText(/something goes wrong/i)).toHaveLength(1);
     });
 
-    it("hide next-img-button when it is the last img", () => {
-      props = createTestProps({
-        activeImgIndex: mockPhotoData.length - 1
+    describe("control panel", () => {
+      it("hide previous-img-button when the first img is active", () => {
+        props = createTestProps({ activeImgIndex: 0 });
+        const { getByText } = mockRender(props);
+        const previousImgBtn = getByText(/\u25C0/i);
+        expect(previousImgBtn).toHaveClass("button__hide");
       });
-      const { getByTestId } = mockRender(props);
 
-      expect(getByTestId("next-img-button")).toHaveClass("button__hide");
+      it("hide next-img-button when the last img is active", () => {
+        props = createTestProps({
+          activeImgIndex: mockPhotoData.length - 1
+        });
+        const { getByText } = mockRender(props);
+        const nextImgBtn = getByText(/\u25B6/i);
+        expect(nextImgBtn).toHaveClass("button__hide");
+
+        // using getByText can imitate better users' exp
+        // const { getByTestId } = mockRender(props);
+        // expect(getByTestId("next-img-button")).toHaveClass("button__hide");
+      });
+
+      it("the indicators (thumbnails) at `activeImgIndex` position should be in active style", () => {
+        props = createTestProps({});
+        const { getByAltText } = mockRender(props);
+        const activeImg = getByAltText(
+          `${mockPhotoData[randomImgIndex].alt_description}_thumbnails`
+        );
+        expect(activeImg.className).toEqual(`panel__thumbnails active`);
+      });
+
+      it("url of indicators (thumbnails) should match with urls.small", () => {
+        props = createTestProps({});
+        const { getAllByRole } = mockRender(props);
+        const images = getAllByRole("img", { class: "panel__thumbnails" });
+
+        expect(images[randomImgIndex].src).toEqual(
+          mockPhotoData[randomImgIndex].urls.small
+        );
+      });
     });
 
-    it("url of display image should be big image and match with the url building logic", () => {
-      const randomImgIndex = Math.floor(
-        Math.random() * Math.floor(mockPhotoData.length)
-      );
-      props = createTestProps({
-        activeImgIndex: randomImgIndex
+    describe("display images in carousel", () => {
+      let getByAltText;
+      beforeEach(() => {
+        props = createTestProps({});
+        let component = mockRender(props);
+        getByAltText = component.getByAltText;
       });
 
-      const { getByAltText } = mockRender(props);
-      const activeImg = getByAltText(
-        mockPhotoData[randomImgIndex].alt_description
-      );
-      expect(activeImg.className).toEqual(`carousel__img active`);
+      it("url of display images should match with urls.regular", () => {
+        const altText = mockPhotoData[randomImgIndex].alt_description;
+        const activeImg = getByAltText(altText);
+        expect(activeImg.src).toEqual(
+          mockPhotoData[randomImgIndex].urls.regular
+        );
+      });
+
+      it("display Img at the position of `activeImgIndex` should have active styling", () => {
+        const altText = mockPhotoData[randomImgIndex].alt_description;
+        const activeImg = getByAltText(altText);
+        expect(activeImg.className).toEqual(`carousel__img active`);
+      });
     });
   });
 
   describe("onclick function", () => {
-    let getByTestId;
+    let getByText;
     let component;
     beforeEach(() => {
       props = createTestProps({});
       component = mockRender(props);
-      getByTestId = component.getByTestId;
+      getByText = component.getByText;
     });
 
     afterEach(() => {
-      mockCallBack.mockClear();
+      mockHandlePhotoChange.mockClear();
     });
 
-    it("previous-img-button click should fire handlePhotoChange events", () => {
-      const previousImgBtn = getByTestId("previous-img-button");
+    it("onclick on `previous-img-button` should trigger `handlePhotoChange` fn", () => {
+      const previousImgBtn = getByText(/\u25C0/i);
 
       fireEvent.click(previousImgBtn);
-      expect(mockCallBack.mock.calls.length).toEqual(1);
+      expect(mockHandlePhotoChange.mock.calls.length).toEqual(1);
+      expect(mockHandlePhotoChange).toHaveBeenCalledWith(randomImgIndex - 1);
     });
 
-    it("previous-img-button click should fire handlePhotoChange events", () => {
-      const nextImgBtn = getByTestId("next-img-button");
+    it("onclick on `previous-img-button` should trigger `handlePhotoChange` fn", () => {
+      const nextImgBtn = getByText(/\u25B6/i);
 
       fireEvent.click(nextImgBtn);
-      expect(mockCallBack.mock.calls.length).toEqual(1);
+      expect(mockHandlePhotoChange.mock.calls.length).toEqual(1);
+      expect(mockHandlePhotoChange).toHaveBeenCalledWith(randomImgIndex + 1);
+    });
+
+    it("onclick on indicators (thumbnails) should trigger `handlePhotoChange` events & pass its index as param", () => {
+      const getAllByRole = component.getAllByRole;
+      const images = getAllByRole("img", { class: "panel__thumbnails" });
+      const selectedIndictor = images[randomImgIndex];
+
+      fireEvent.click(selectedIndictor);
+      expect(mockHandlePhotoChange.mock.calls.length).toEqual(1);
+      expect(mockHandlePhotoChange).toHaveBeenCalledWith(randomImgIndex);
     });
   });
 });
